06349a21e84ccf2c9d57594b08f6921ab9b5db03
jeonyul <jeonyul00@gmail.com>
2022-08-10 13:21:45 +0900

    나중에 풀게요 지금은 생각하기 싫어요

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import EmptyNumber from "./pages/220804";
 import Add from "./pages/220807";
 import Inner from "./pages/220809";
+import Decimal from "./pages/220810";
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
       <Add />
       <p>-----------------------------------------</p>
       <Inner />
+      <p>-----------------------------------------</p>
+      <Decimal />
     </>
   );
 }
